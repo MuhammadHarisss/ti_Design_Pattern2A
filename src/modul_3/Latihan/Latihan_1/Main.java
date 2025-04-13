40c654c37f263e73abfab664d1dcf4dcb6128263
MuhammadHarisss <haris.muhammad1302@gmail.com>
2025-04-13 17:26:05 +0700

    Quick review - 4 Pillar OOP Menggunakan Java

---
+++ src/modul_3/Latihan/Latihan_1/Main.java
@@ -0,0 +1,8 @@
+package modul_3.Latihan.Latihan_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Buku buku1 = new Buku("Algoritma dan Struktur Data", "Dedi Supriadi", 2022);
+        buku1.tampilkanInfo();
+    }
+}
